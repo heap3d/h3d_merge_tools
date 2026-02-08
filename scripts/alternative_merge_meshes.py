bf5eeb209af1dba5d85e812f89e165c2c27149b3
Dmytro Holub <heap3d@gmail.com>
2026-02-08 03:22:48 +0200

    updated: UI refactored: alternative merge meshes function added: merge meshes function added: safe merge meshes function

--- scripts/alternative_merge_meshes.py
+++ scripts/alternative_merge_meshes.py
@@ -10,20 +10,16 @@
 
 import modo
 import modo.constants as c
-import lx
+
+from h3d_merge_tools.scripts.safe_merge import safe_merge_meshes
 
 
 def main():
-    selected_meshes = modo.Scene().selectedByType(itype=c.MESH_TYPE)
-    source_items = selected_meshes[:-1]
-    target_item = selected_meshes[-1]
+    selected_meshes: list[modo.Mesh] = modo.Scene().selectedByType(itype=c.MESH_TYPE)
+    source_items: list[modo.Mesh] = selected_meshes[:-1]
+    target_item: modo.Item = selected_meshes[-1]
 
-    modo.Scene().deselect()
-    modo.Scene().select(target_item)
-    for item in source_items:
-        item.select()
-
-    lx.eval('layer.mergeMeshes true')
+    safe_merge_meshes(target_item, source_items)
 
 
 if __name__ == "__main__":
--- scripts/safe_merge.py
+++ scripts/safe_merge.py
@@ -10,10 +10,46 @@
 
 import modo
 import modo.constants as c
+import lx
+
+from h3d_utilites.scripts.h3d_utils import get_user_value
+
+
+USERVAL_VMAP_NORMAL_PERFECT_NAME = "h3d_mrgt_vmap_normal_perfect_name"
 
 
 def main():
-    selected_meshes = modo.Scene().selectedByType(itype=c.MESH_TYPE)
+    selected_meshes: list[modo.Mesh] = modo.Scene().selectedByType(itype=c.MESH_TYPE)
+    if len(selected_meshes) < 2:
+        lx.out("Select at least 2 meshes to merge.")
+        return
+
+    vmap_normal_perfect_name = get_user_value(USERVAL_VMAP_NORMAL_PERFECT_NAME)
+
+    target_mesh = selected_meshes[0]
+    meshes = selected_meshes[1:]
+
+    safe_merge_meshes(target_mesh, meshes)
+
+
+def safe_merge_meshes(target_mesh: modo.Mesh, meshes: list[modo.Mesh]):
+
+    vmap_normal_maps = set()
+    for mesh in [target_mesh, *meshes]:
+        vmap_normal_maps.update(mesh.geometry.vmaps.getMapsByType(lx.symbol.i_VMAP_NORMAL))
+
+    print(vmap_normal_maps)
+
+    merge_meshes(target_mesh, meshes)
+
+
+def merge_meshes(target_mesh: modo.Mesh, meshes: list[modo.Mesh]):
+    modo.Scene().deselect()
+    target_mesh.select()
+    for mesh in meshes:
+        mesh.select()
+
+    lx.eval('layer.mergeMeshes true')
 
 
 if __name__ == "__main__":
