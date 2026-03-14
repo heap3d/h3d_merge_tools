65c723f8171acee51d9bda1480c82b4635259234
Dmytro Holub <heap3d@gmail.com>
2026-03-14 18:25:10 +0200

    added: vertex narmal maps functionality

--- scripts/alternative_merge_meshes.py
+++ scripts/alternative_merge_meshes.py
@@ -10,14 +10,14 @@
 
 import modo
 
-from h3d_utilites.scripts.h3d_utils import get_user_value
-
 from h3d_merge_tools.scripts.safe_merge import (
-    DEFAULT_VMAP_NORMAL_PERFECT_NAME,
-    USERVAL_VMAP_NORMAL_PERFECT_NAME,
     SELECTED_TYPES,
+    initialize_env,
     safe_merge_meshes,
+    stats_processing,
 )
+
+from h3d_utilites.scripts.h3d_debug import h3dd
 
 
 def main():
@@ -29,13 +29,13 @@ def main():
     target_mesh: modo.Item = selected_items[-1]
     merging_meshes: list[modo.Item] = selected_items[:-1]
 
-    vmap_normal_perfect_name = get_user_value(USERVAL_VMAP_NORMAL_PERFECT_NAME)
-    if not vmap_normal_perfect_name:
-        vmap_normal_perfect_name = DEFAULT_VMAP_NORMAL_PERFECT_NAME
+    env = initialize_env()
 
-    safe_merge_meshes(target_mesh, merging_meshes, vmap_normal_perfect_name)
+    stats = safe_merge_meshes(target_mesh, merging_meshes, env.vmap_normal_perfect_name)
+
+    stats_processing(stats, env)
 
 
 if __name__ == '__main__':
-    # h3dd.enable_debug_output()
+    h3dd.enable_debug_output()
     main()
